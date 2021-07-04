9bf8d5e7c39af4c5e16bfc81efa57868cf861fef
Anurag <anuragak07@gmail.com>
2021-07-05 01:47:13 +0530

    update

---
+++ app/src/main/java/com/masai/eventlisteners/MainActivity.java
@@ -0,0 +1,39 @@
+package com.masai.eventlisteners;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mtvSaveInstance;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mtvSaveInstance = findViewById(R.id.tvSaveinstance);
+        mtvSaveInstance.setText("Welcome to Masai School");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("Key","Screen Rotated");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String data = savedInstanceState.getString("Key");
+        mtvSaveInstance.setText(data);
+    }
+}
